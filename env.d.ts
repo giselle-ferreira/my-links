036aceacadbaca091a67cb6dacee1e3e75f59f44
Giselle Ferreira <giselle.fs@gmail.com>
2024-03-18 18:50:00 -0300

    updates env var

--- env.d.ts
+++ env.d.ts
@@ -1,9 +1,9 @@
-interface ImportMeta {
-    readonly env: ImportMetaEnv
-}
+// interface ImportMeta {
+//     readonly env: ImportMetaEnv
+// }
 
-interface ImportMetaEnv {
-    readonly VITE_SERVICE_ID: string;
-    readonly VITE_TEMPLATE_ID: string;
-    readonly VITE_PUBLIC_KEY: string;
-}
+// interface ImportMetaEnv {
+//     readonly SERVICE_ID: string;
+//     readonly TEMPLATE_ID: string;
+//     readonly PUBLIC_KEY: string;
+// }
--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import ImportMetaEnvPlugin from '@import-meta-env/unplugin';
+// import ImportMetaEnvPlugin from '@import-meta-env/unplugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    ImportMetaEnvPlugin.vite({
-      example: ".env",
-    }),
-    react()
+    // ImportMetaEnvPlugin.vite({
+    //   example: ".env",
+    // }),
+    react(),
   ]
 })
-
-
-// export default defineConfig({
-//   plugins: [
-//     react()
-//   ],
-//   define: {
-//     "import.meta.env": import.meta.env,
-//     SERVICE_ID: import.meta.env.SERVICE_ID,
-//     TEMPLATE_ID: import.meta.TEMPLATE_ID,
-//     PUBLIC_KEY: import.meta.PUBLIC_KEY,
-//   },
-// })
-
 
